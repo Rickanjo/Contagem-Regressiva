642292b3be27bbfa0193a6c5b2e68d7ba7aa4a86
Rickanjo <ricardo.cassettari07@gmail.com>
2023-03-08 13:59:41 -0300

    Exercício Resolvido

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Contagem-Regressiva");
+Console.WriteLine("");
+
+
+Console.ReadKey();
+Console.WriteLine("Preparar...");
+Console.ReadKey();
+Console.WriteLine("Apontar...");
+Console.ReadKey();
+Console.WriteLine("FOGO!");
